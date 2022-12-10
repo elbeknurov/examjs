7c34fd65941d0dc8aecaec46afe2ae834d4bf373
elbeknurov <elbeknurov11@gmail.com>
2022-12-10 18:10:53 +0500

    last

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // it`s 2rd example
+// function palendrom(n) {
+//   const num = String(n);
+//   console.log(nume);
+//   const array = num.split("");
+//   console.log(array);
+//   const rever = array.reverse();
+//   console.log(rever);
+//   const join = rever.join("");
+//   console.log(join);
+//   const number = Number(join);
+//   console.log(number);
+//   if (n === number) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-function palendrom(n) {
-  const num = String(n);
-  console.log(nume);
-  const array = num.split("");
-  console.log(array);
-  const rever = array.reverse();
-  console.log(rever);
-  const join = rever.join("");
-  console.log(join);
-  const number = Number(join);
-  console.log(number);
-  if (n === number) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(palendrom(454));
+// console.log(palendrom(454));
 
 // it`s 3rd example
 
@@ -31,3 +30,16 @@ console.log(palendrom(454));
 // }
 
 // console.log(kabisa(1200));
+
+// it`s 4rd example
+// var t = [-1, -2, -3, 5, 6, 1];
+// var negativeArr = [];
+
+// t.forEach(function (item) {
+//   if (item < 0) {
+//     negativeArr.push(item);
+//   }
+// });
+
+// console.log(positiveArr);
+// console.log(negativeArr);
